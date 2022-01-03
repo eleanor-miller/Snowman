ca8d599b55da1bfa58c150ac432e2e05ca824a1d
eleanor-miller <eleanormiller08@icloud.com>
2022-01-03 16:45:18 -0500

    submit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,250 @@
 import React from 'react'
+import { MouseEvent, useState } from 'react'
+import './index.scss'
+import wordList from './words.json'
 
-export function App() {
-  return <div>Hello, World </div>
+function App() {
+  const [word] = useState(wordList[Math.floor(Math.random() * 1024)]) // TODO : Pick random word from list
+  const [blanks, setBlanks] = useState(() => {
+    let someString = ''
+    for (let i = 0; i < word.length; i++) {
+      someString += '_'
+    }
+    return someString
+  })
+
+  const snowmen = [
+    <img src={'./src/snowman/step_0.png'} className="App-logo" />,
+    <img src={'./src/snowman/step_1.png'} className="App-logo" />,
+    <img src={'./src/snowman/step_2.png'} className="App-logo" />,
+    <img src={'./src/snowman/step_3.png'} className="App-logo" />,
+    <img src={'./src/snowman/step_4.png'} className="App-logo" />,
+    <img src={'./src/snowman/step_5.png'} className="App-logo" />,
+    <img src={'./src/snowman/step_6.png'} className="App-logo" />,
+    <img src={'./src/snowman/step_7.png'} className="App-logo" />,
+  ]
+  const [matches, setMatches] = useState(0)
+  const [snowman, setSnowman] = useState(snowmen[matches])
+
+  function handleClickLetter(event: MouseEvent) {
+    const letter = event.target as HTMLButtonElement
+    let l = letter.innerHTML
+    var tempString = blanks
+    var tempMatches = matches
+
+    //reveal spaces with matching letters
+    for (let i = 0; i < word.length; i++) {
+      if (l.toLowerCase() == word.charAt(i).toLowerCase()) {
+        tempString =
+          tempString.substring(0, i) + l + tempString.substring(i + 1)
+
+        tempMatches = tempMatches + 1 //update snowman
+      }
+    }
+    setMatches(tempMatches)
+    setBlanks(tempString)
+    setSnowman(snowmen[tempMatches])
+
+    //Disable the letter that was clicked
+    letter.className = 'disabled'
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {snowman}
+        <p>{blanks}</p>
+        <div>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            Q
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            W
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            E
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            R
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            T
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            Y
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            U
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            I
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            O
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            P
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            A
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            S
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            D
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            F
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            G
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            H
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            J
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            K
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            L
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            Z
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            X
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            C
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            V
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            B
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            N
+          </button>
+          <button
+            onClick={(event) => {
+              handleClickLetter(event as unknown as MouseEvent)
+            }}
+          >
+            M
+          </button>
+        </div>
+      </header>
+    </div>
+  )
 }
+
+export default App
